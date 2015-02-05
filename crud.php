9582520123c757e8677cbbf0438b977a32d7d117
Lepeltier kévin <kevin@lepeltier.info>
2015-02-05 22:43:20 +0100

    next

--- crud.php
+++ crud.php
@@ -1,57 +1,81 @@
 <?php
     header("Cache-Control: max-age=1");
-    header("Access-Control-Allow-Origin: *");
     
-    $debug = $_REQUEST['debug'];
-    if( $debug ) {
+    if( $_REQUEST['debug'] ) {
         ini_set('display_errors', 1);
         ini_set('log_errors', 1);
         ini_set('error_log', dirname(__file__) . '/log_error_php.txt');
         error_reporting(E_ALL);
         echo "version 5"."<br/>";
     }
+    
+    function d ( $obj, $det = false ) {
+        if( $_REQUEST['debug'] && $det )
+            echo print_r($obj, true)."<br/>";
+        else if( $_REQUEST['debug'] )
+            echo $obj."<br/>";
+    }
 ?>
 
 <?php
 
 // db sqlite
-$db = new SQLite3(dirname(__FILE__).'/db/gaia.sqlite');
-if( $debug ) echo print_r($db, true)."<br/>";
+$db = new SQLite3(dirname(__FILE__).'/db/gaia.sqlite');                         d($db, true);
 
 // action
-$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
-if( $debug ) echo $action."<br/>";
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';                d($action);
 
 if( isset($_REQUEST['login']) && $action == 'user_exist' ) {
     
-    $login = $_REQUEST['login'];
-    if( $debug ) echo $login.'<br/>';
+    $login = stripslashes($_REQUEST['login']);                                  d($login);
     
-    $query = "SELECT rank, login FROM user WHERE login = '".stripslashes($login)."';";
-    if( $debug ) echo $query.'<br/>';
+    $query = "SELECT * FROM user WHERE login = '".$login."';";                  d($query);
+    $res = $db->query($query);                                                  d($res, true);
+    $rest = $res->fetchArray();                                                 d($rest, true);
     
-    $res = $db->query($query);
-    if( $debug ) echo print_r($res, true)."<br/>";
-    
-    $rest = $res->fetchArray();
-    if( $debug ) echo print_r($rest, true)."<br/>";
-    
-    if ( is_array($rest) ) {
-        if( $debug ) echo $login." exist".'<br/>';
-        if( $debug ) echo 'rank '.$rest['rank'].'<br/>';
+    if ( is_array($rest) ) {                                                    d($login." exist");
+        $rest['reply'] = true;
+    } else {                                                                    d($login." unexist");
         
-        $results = new stdClass();
-        $results->login = $login;
-        $results->rank = $rest['rank'];
-    } else {
-        if( $debug ) echo $login." unexist".'<br/>';
+        $req =  'INSERT INTO user '.
+                '(login, `group`, create_date, last_login_date, suspend, rank) '.
+                'VALUES ("'.$login.'", "", datetime(), datetime(), 0, 1);';     d($req);
+        $do = $db->exec($req);                                                  d($do, true);
+    
+        $query = "SELECT * FROM user WHERE login = '".$login."';";              d($query);
+        $res = $db->query($query);                                              d($res, true);
+        $rest = $res->fetchArray();                                             d($rest, true);
         
-        $results = new stdClass();
-        $results->login = $login;
-        $results->rank = 0;
+        $rest['reply'] = false;
     }
     
-    echo json_encode ( $results );
+    if( isset($_REQUEST['X']) && isset($_REQUEST['Y']) ) {
+    
+        $X = stripslashes($_REQUEST['X']);                                      d($X);
+        $Y = stripslashes($_REQUEST['Y']);                                      d($Y);
+        
+        $req = 'INSERT INTO position '.
+               '(user, X, Y, date, time) '.
+               'VALUES ("'.$login.'", '.$X.', '.$Y.', date(), datetime());';    d($req);
+        $do = @$db->exec($req);                                                 d($do, true);
+    
+    }
+    
+    echo json_encode ( $rest );
+}
+
+if( isset($_REQUEST['login']) && $action == 'user_move' ) {
+    
+    $login = stripslashes($_REQUEST['login']);                                  d($login);
+    
+    $query = "SELECT * FROM position ".
+             "WHERE user = '".$login."' ORDER BY time ASC;";                    d($query);
+    $res = $db->query($query);                                                  d($res, true);
+    $rest = [];
+    while($row=$res->fetchArray())
+        $rest[] = $row;                                                         d($rest, true);
+    
+    echo json_encode ( $rest );
 }
 
 ?>
--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 //debug 
 var debug = window.location.toString().indexOf('localhost') != -1 ? true : false;
-debug = false;
+//debug = false;
 if(debug) console.log('debug');
 
 $( document ).ready(function() {
@@ -19,31 +19,14 @@ $( document ).ready(function() {
         login: get.p,
         X: Number(coord[0]),
         Y: Number(coord[1]),
-        rank:0
+        rank: 0,
+        group: '',
+        create_date: 0,
+        last_login_date: 0,
+        suspend: 0
     };
     
-    CRUD.isUserExist( {login:user.login}, initGaia );
-    
-    function initGaia (data){
-        
-        console.log('Retour de user_exist');
-        console.log(data);
-        
-        if( data.rank == 0 ) {
-            console.log('new');
-            user.rank = 1;
-            $("#welcome").show('slow');
-        } else {
-            console.log('old');
-            user.rank = Number(data.rank);
-            $( "#butReport" ).click();
-        }
-        
-        var rank = $("#rank").attr('src', "img/rank"+user.rank+".png");
-        rank.attr('alt', user.rank+" (GAIA)");
-        $("#login").text(user.login);
-        
-    }
+    CRUD.isUserExist( {login:user.login, X:user.X, Y:user.Y}, initGaia );
     
     // gaia
     
@@ -54,9 +37,84 @@ $( document ).ready(function() {
         $(".toggle_container").hide('slow');
         $( $(this).attr('href') ).show('slow');
         
+        switch ( $(this).attr('id') ) {
+            case 'butReport' : report(); break;
+            case 'butMap' : map(); break;
+        }
+        
         return false;
         
     });
+    
+    function initGaia (data){
+        
+        console.log('Retour de user_exist');
+        console.log(data);
+        
+        user.group = data.group;
+        user.create_date = new Date(data.create_date);
+        user.last_login_date = new Date(data.last_login_date);
+        user.suspend = !!Number(data.suspend);
+        user.rank = Number(data.rank);
+        
+        console.log(user);
+        
+        if( !data.reply ) {
+            console.log('new');
+            $("#welcome").show('slow');
+        } else {
+            console.log('old');
+            $( "#butReport" ).click();
+        }
+        
+        var rank = $("#rank").attr('src', "img/rank"+user.rank+".png");
+        rank.attr('alt', user.rank+" (GAIA)");
+        $("#login").text(user.login);
+        $("#coord").text('['+user.X+']['+user.Y+']');
+        
+        var msecPerDay = 1000 * 60 * 60 * 24;
+        var interval = new Date().getTime() - user.create_date.getTime();
+        var days = Math.floor(interval / msecPerDay );
+        
+        $("#date").text(days+' jours');
+        
+    }
+    
+    function report () {
+        
+        alert( 'report' );
+        alert( this );
+        
+    }
+    
+    function map () {
+        
+        alert( 'map' );
+        alert( this );
+    
+        CRUD.getMove( {login:user.login}, initUserMove );
+        
+    }
+    
+    function initUserMove (data) {
+        
+        console.log('Retour de User Move');
+        console.log(data);
+        
+        for (var i=0, pos=[] ; i < data.length ; i++) {
+            pos[i] = {};
+            pos[i].X = data[i].X;
+            pos[i].Y = data[i].Y;
+            pos[i].time = data[i].time;
+            
+            $("#position").append('<li>'+pos[i].time+'</li>')
+            
+        }
+        
+        console.log(pos);
+            
+        
+    }
     
 });
 
@@ -64,6 +122,14 @@ var CRUD = {
     
     isUserExist: function( vars, success, success2, fail, always ) {
         vars.action = 'user_exist';
+        $.getJSON( debug ? 'crud.php' : 'http://a4edfd900b.url-de-test.ws/gaia/crud.php', vars, success )
+        .done(success2)
+        .fail(fail)
+        .always(always);
+    },
+    
+    getMove: function( vars, success, success2, fail, always ) {
+        vars.action = 'user_move';
         $.getJSON( debug ? 'crud.php' : 'http://a4edfd900b.url-de-test.ws/gaia/crud.php', vars, success )
         .done(success2)
         .fail(fail)

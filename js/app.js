e413d4eaa9a879efe48dc6695a2d9d1f569004b3
Lepeltier kévin <kevin@lepeltier.info>
2015-02-09 15:01:14 +0100

    noiframe

--- js/app.js
+++ js/app.js
@@ -1,168 +1,202 @@
+function GAIA () {
+    
+    //debug
+    if(GAIADebug) console.log('debug');
+    
+    // link css
+    var lien_css = document.createElement('link');
+    lien_css.href = 'http://lipki.github.io/GAIA-Report/css/style.css';
+    if(GAIADebug)
+    lien_css.href = "http://localhost/GAIA-Report/css/style.css";
+    lien_css.rel = "stylesheet";
+    lien_css.type = "text/css";
+    document.getElementsByTagName("head")[0].appendChild(lien_css);
+    
+    // get elements
+    this.first = false;
+    this.dom = {};
+    this.dom.iframe = document.getElementById('iframe');
+    this.dom.innerDoc = this.dom.iframe.contentDocument || this.dom.iframe.contentWindow.document;
+    var menu = this.dom.innerDoc.getElementById('menu');
+    var liMaPage = menu.getElementsByTagName('li')[1];
+    var aMaPage = liMaPage.getElementsByTagName('a')[0];
 
-//debug 
-var debug = window.location.toString().indexOf('localhost') != -1 ? true : false;
-//debug = false;
-if(debug) console.log('debug');
+    //install
+    var center = document.getElementById('center');
+    var onglet = this.dom.onglet = document.createElement('button');
+    onglet.setAttribute('id', 'gaia');
+    center.appendChild(onglet);
+    center.insertBefore(onglet, this.dom.iframe);
+    onglet.style.display = 'block';
+    
+    // event
+    var _this = this;
+    if (onglet.addEventListener) onglet.addEventListener('click', function(){_this.gaiaSwitch()}, false);
+    else if (onglet.attachEvent) onglet.attachEvent('onclick', function(){_this.gaiaSwitch()});
 
-$( document ).ready(function() {
-    
-    // post gaia
-    var vars = window.location.search.substring(1).split("&");
-    for (var i=0, get={} ;i<vars.length;i++) {
-        var va = vars[i].split('=');
-        get[va[0]] = va[1];
-    }
-    
-    var coord = get.c.split('][').join(',').split(']').join('').split('[').join('').split(',');
-    
-    var user = {
-        login: get.p,
-        X: Number(coord[0]),
-        Y: Number(coord[1]),
-        rank: 0,
-        group: '',
-        create_date: new Date(),
-        last_login_date: new Date(),
-        suspend: 0
-    };
-    
-    console.log(user);
-    
-    CRUD.call( {login:user.login}, 'isUserExist', initGaia );
-    
-    // gaia
-    
-    $( "a.butmenu" ).click(function( event ) {
+    if (this.dom.iframe.addEventListener) this.dom.iframe.addEventListener('load', function(){_this.pageLoad()}, false);
+    else if (this.dom.iframe.attachEvent) this.dom.iframe.attachEvent('onload', function(){_this.pageLoad()});
         
-        $(this).toggleClass("active");
+    // set user
+    this.user = {};
+    this.user.login = '';
+    this.user.X = 0;
+    this.user.Y = 0;
+    this.user.rank = 0;
+    this.user.group = '';
+    this.user.create_date = new Date();
+    this.user.last_login_date = new Date();
+    this.user.suspend = 0;
+    this.user.linkMaPage = aMaPage.href;
+
+    //start
+    this.gaiaSwitch(null);
+    
+};
+
+GAIA.prototype.gaiaSwitch = function(evt) {
+    console.log('switch');
+    this.dom.iframe.src = this.user.linkMaPage;
+};
+
+GAIA.prototype.pageLoad = function(evt) {
+    console.log('load');
         
-        $(".toggle_container").hide('slow');
-        $( $(this).attr('href') ).show('slow');
+    var dom = this.dom, user = this.user;
+
+    var href = window.open('','iframe').location.href;
+    if(    href.indexOf('/user/') != -1
+        && href.indexOf('/ranking/') == -1
+        && href.indexOf('/missions/') == -1
+        && href.search(/\/$/) == -1) {
         
-        switch ( $(this).attr('id') ) {
-            case 'butReport' : report(); break;
-            case 'butMap' : map(); break;
+        var iframe   = dom.iframe   = document.getElementById('iframe');
+        var innerDoc = dom.innerDoc = dom.iframe.contentDocument || dom.iframe.contentWindow.document;
+        var section = dom.innerDoc.getElementById('section');
+        var card = section.getElementsByClassName('card')[0];
+        var a = card.getElementsByTagName('a')[0];
+        
+        if( !this.first ) {
+            user.login = a.textContent;
+            this.testUser();
         }
         
-        return false;
+        this.dom.onglet.style.backgroundImage = 'url(http://lipki.github.io/GAIA-Report/img/tab_on.gif)';
+
+        active = innerDoc.getElementsByClassName('active')[0];
+        if( active != undefined ) active.className = '';
         
-    });
-    
-    function initGaia (reply){
+        /*var menu     = dom.menu     = dom.innerDoc.getElementById('menu');
+        var liMaPage = dom.liMaPage = menu.getElementsByTagName('li')[1];
+        var aMaPage  = dom.aMaPage  = liMaPage.getElementsByTagName('a')[0];*/
         
-        console.log('Retour de user_exist');
-        console.log(reply);
+        var span = card.getElementsByTagName('span')[0];
+        var coord = span.textContent;
+            coord = coord.split('][').join(',').split(']').join('').split('[').join('').split(',');
         
-        //for (var a in user) user[a] = reply.data[0][a];
-        user.rank = reply.data[0].rank;
-        user.create_date = new Date(reply.data[0].create_date.split(' ').join('T'));
-        user.last_login_date = new Date(reply.data[0].last_login_date.split(' ').join('T'));
+        var X = user.X = Number(coord[0]);
+        var Y = user.Y = Number(coord[1]);
         
-        var rank = $("#rank").attr('src', "img/rank"+user.rank+".png");
-        rank.attr('alt', user.rank+" (GAIA)");
-        $("#login").text(user.login);
-        $("#coord").text('['+user.X+']['+user.Y+']');
+        user = section.getElementsByClassName('user')[0];
+        card = user.getElementsByClassName('card')[0];
+
+        user.innerHTML = null;
+        user.appendChild(card);
         
-        var msecPerDay = 1000 * 60 * 60 * 24;
-        var interval = new Date().getTime() - user.create_date.getTime();
-        var days = Math.floor(interval / msecPerDay );
+        a.href = a.href+'/';
         
-        $("#date").text(days+' jours');
-        
-        if( !reply.success ) {
-            console.log('new');
-            $("#welcome").show('slow');
-        } else {
-            console.log('old');
-            $( "#butReport" ).click();
-        }
-        
+    } else {
+        dom.onglet.style.backgroundImage = 'url(http://lipki.github.io/GAIA-Report/img/tab_off.gif)';
     }
+};
+
+GAIA.prototype.testUser = function() {
+    console.log('CRUD');
+    CRUD.call( {login:this.user.login}, 'isUserExist', this, 'isUserExist' );
+};
+
+GAIA.prototype.isUserExist = function( reply ) {
     
-    function report () {
-        console.log( 'report' );
-        CRUD.call( {X:user.X, Y:user.Y}, 'getCellInfo', initReport );
+    console.log('Retour de user_exist');
+    console.log(reply);
+    
+    var user = this.user;
+    user.rank = reply.data[0].rank;
+    user.create_date = new Date(reply.data[0].create_date.split(' ').join('T'));
+    user.last_login_date = new Date(reply.data[0].last_login_date.split(' ').join('T'));
+    
+    var rank = $("#rank").attr('src', "img/rank"+user.rank+".png");
+    rank.attr('alt', user.rank+" (GAIA)");
+    $("#login").text(user.login);
+    $("#coord").text('['+user.X+']['+user.Y+']');
+    
+    var msecPerDay = 1000 * 60 * 60 * 24;
+    var interval = new Date().getTime() - user.create_date.getTime();
+    var days = Math.floor(interval / msecPerDay );
+    
+    $("#date").text(days+' jours');
+    
+    if( !reply.success ) {
+        console.log('new');
+        var pop = this.dom.pop = document.getElementById('pop');
+        var innerpop = this.dom.innerpop = document.getElementById('inner');
+        pop.style.display = 'block';
+        innerpop.innerHTML = ''+
+        '<div class="GAIAMissionStart">'+
+            '<div class="mission">'+
+                '<div id="mDate" class="date"></div>'+
+                '<h3>GAIA Report</h3>'+
+                '<div class="content">'+
+                    '<div class="pic">'+
+                        '<img src="http://lipki.github.io/GAIA-Report/img/missiongaia.png" alt="Img">'+
+                    '</div>'+
+                    'Bien joué détenu <span id="mUser"></span>.'+
+                    "Grâce à votre aide les laboratoires d'ESCorp vont pouvoir mettre la main sur des informations précieuses."+
+                    "<strong>Le programme d'extension</strong>"+
+                    "vient d'être transféré comme promis non loin de votre secteur d'origine <em>[-1][0]</em>."+
+                '</div>'+
+            '</div>'+
+        '</div>';
+    } else {
+        console.log('old');
+        ///$( "#butReport" ).click();
     }
+
+    //pop.style.display = 'block';
+    //innerpop.innerHTML = 'pouf<br/>pouf';
     
-    function map () {
-        console.log( 'map' );
-        CRUD.call( {login:user.login}, 'getUserMove', initUserMove );
-    }
+    //for (var a in user) user[a] = reply.data[0][a];
     
-    function initUserMove (reply) {
-        
-        console.log('Retour de User Move');
-        console.log(reply);
-        
-        for (var i=0, pos=[] ; i < reply.data.length ; i++) {
-            pos[i] = {};
-            pos[i].X = reply.data[i].X;
-            pos[i].Y = reply.data[i].Y;
-            
-            var time = new Date(reply.data[i].time.split(' ').join('T'))
-            pos[i].time = time.toLocaleString();
-            
-            $("#position").append('<li>'+'['+pos[i].X+']['+pos[i].Y+']<span>'+pos[i].time+'</span></li>')
-            
-        }
-        
-        console.log(pos);
-            
-    }
-    
-    function initReport (reply) {
-        
-        console.log('Retour de get Info');
-        console.log(reply);
-            
-        $("#space p.submit input[type='submit']")
-            .val("Modifier ou valider ce secteur")
-            .unbind("click")
-            .bind("click", (function (evt) {
-                evt.preventDefault();
-                var cell = {
-                    mineral: $("#mineral").val(),
-                    missile: $("#missile").val(),
-                    antiradar: $("#antiradar").val(),
-                    bonusp: $("#bonusp").val(),
-                    X: user.X,
-                    Y: user.Y,
-                    user: user.login
-                };
-                CRUD.call( cell, 'setCellInfo', initReport );
-            }));
-        
-        if( reply.data.length > 0 ) {
-            
-            $("#mineral").val(reply.data[0].mineral);
-            $("#missile").val(reply.data[0].missile);
-            if( reply.data[0].antiradar )
-                $("#antiradar").attr('checked', true);
-            $("#bonusp").val(reply.data[0].bonus);
-            $("#X").val(user.X);
-            $("#Y").val(user.Y);
-            $("#user").val(user.login);
-            
-            $("#space p.submit input[type='submit']").val("Modifier ou valider ce secteur");
-            
-        } else {
-            $("#space p.submit input[type='submit']").val("Ajouter ce secteur");
-        }
-            
-    }
-    
-    /*
-    
-    */
-    
-});
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 var CRUD = {
     
-    call: function( vars, action, success ) {
+    call: function( vars, action, _this, success ) {
         vars.action = action;
-        $.getJSON( debug ? 'crud.php' : 'http://a4edfd900b.url-de-test.ws/gaia/crud.php', vars, success )
-        .fail(CRUD.fail)
+        
+        var data = [];
+        for( var va in vars ) data.push(va+'='+vars[va]);
+    
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", function(){ _this[success](JSON.parse(xhr.responseText)) }, false);
+        xhr.addEventListener("error", function(){CRUD.fail()}, false);
+        //xhr.open('POST', 'http://127.0.0.1/GAIA-Report/crud.php');
+        xhr.open('GET', 'http://a4edfd900b.url-de-test.ws/gaia/crud.php?'+data.join('&'));
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");  
+        xhr.send();
+        
     },
     
     success2: function reportReturn (data) {
@@ -181,3 +215,5 @@ var CRUD = {
     }
     
 };
+
+new GAIA();
--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,109 +1,15 @@
 if(top['bookmarkletGAIAreport']) {
    top['bookmarkletGAIAreport'];
 } else {
-  (function(){
-    if(window.location.host.indexOf('alphabounce.com') != -1 && document.getElementById('gaia') == null) {
-      var d = document, iframe, innerDoc, menu, li, onglet, center, card;
-
-      onglet = d.createElement('a');
-      onglet.setAttribute('href', 'http://lipki.github.io/GAIA-Report/gaia.html');
-      onglet.setAttribute('id', 'gaia');
-      onglet.style.position = 'absolute';
-      onglet.style.right = '10px';
-      onglet.style.top = '96px';
-      onglet.style.zIndex = '10';
-      onglet.style.display = 'block';
-      onglet.style.backgroundImage = 'url(http://lipki.github.io/GAIA-Report/img/tab_off.gif)';
-      onglet.style.width = '21px';
-      onglet.style.height = '29px';
-
-      iframe = d.getElementById('iframe');
-      center = d.getElementById('center');
-      center.appendChild(onglet);
-      center.insertBefore(onglet, iframe);
-      center.style.position = 'relative';
-
-      function pageLoad(evt) {
-        var href, newIframe, section, user, active, pseudo, span, coord, a;
-
-        iframe = d.getElementById('iframe');
-        innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-
-        href = window.open('','iframe').location.href;
-        if(   href.indexOf('/user/') != -1
-           && href.indexOf('/ranking/') == -1
-           && href.indexOf('/missions/') == -1
-           && href.search(/\/$/) == -1) {
-          
-          onglet.style.backgroundImage = 'url(http://lipki.github.io/GAIA-Report/img/tab_on.gif)';
-
-          section = innerDoc.getElementById('section');
-          card = section.getElementsByClassName('card')[0];
-          a = card.getElementsByTagName('a')[0];
-          pseudo = a.textContent;
-          span = card.getElementsByTagName('span')[0];
-          coord = span.textContent;
-
-          a = d.getElementById('gaia');
-
-          newIframe = innerDoc.createElement('iframe');
-          newIframe.setAttribute('id', 'gaiaIframe');
-          newIframe.setAttribute('frameborder', '');
-          newIframe.setAttribute('allowtransparency', 'true');
-          newIframe.setAttribute('scrolling', 'no');
-          newIframe.setAttribute('src', a.href+'?p='+pseudo+'&c='+coord);
-          newIframe.style.border = '0';
-          newIframe.style.width = '473px';
-          newIframe.style.height = '387px';
-
-          section.style.overflow = 'hidden';
-          section.style.margin = '0';
-          section.style.width = '473px';
-          section.style.height = '387px';
-          user = section.getElementsByClassName('user')[0];
-          card = user.getElementsByClassName('card')[0];
-
-          user.innerHTML = null;
-          user.appendChild(newIframe);
-
-          menu = innerDoc.getElementById('menu');
-          li = menu.getElementsByTagName('li')[1];
-          a = li.getElementsByTagName('a')[0];
-          a.href = a.href+'/';
-          
-          active = innerDoc.getElementsByClassName('active')[0];
-          if( active != undefined ) active.className = '';
-        } else {
-          onglet.style.backgroundImage = 'url(http://lipki.github.io/GAIA-Report/img/tab_off.gif)';
-        }
-      }
-
-      function gaiaSwitch(evt) {
-        if(evt != null) evt.preventDefault();
-        var iframe, innerDoc, menu, li, a;
-
-        iframe = d.getElementById('iframe');
-        innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-        menu = innerDoc.getElementById('menu');
-        li = menu.getElementsByTagName('li')[1];
-        a = li.getElementsByTagName('a')[0];
-        iframe.src = a.href.replace(/\/$/, '');
-      };
-
-      gaiaSwitch(null);
-
-      if (onglet.addEventListener) onglet.addEventListener('click', gaiaSwitch, false);
-      else if (onglet.attachEvent) onglet.attachEvent('onclick', gaiaSwitch);
-
-      if (iframe.addEventListener) iframe.addEventListener('load', pageLoad, false);
-      else if (iframe.attachEvent) iframe.attachEvent('onload', pageLoad);
-
+  (function(d){
+    if(window.location.host.indexOf('alphabounce.com') != -1 && d.getElementById('gaia') == null) {
+      if (d.getElementById('GAIA')) return;
+      var E = d.createElementNS && d.documentElement.namespaceURI;
+      E = E ? d.createElementNS(E, 'script') : d.createElement('script');
+      E.setAttribute('id', 'GAIA');
+      E.setAttribute('src', 'http://localhost/GAIA-Report/js/app.js');
+      //E.setAttribute('src', 'http://lipki.github.io/GAIA-Report/js/app.js');
+      (d.getElementsByTagName('head')[0] || d.getElementsByTagName('body')[0]).appendChild(E);
     }
-  })();
+  })(document);
 }
-
-/*
-Exception: ReferenceError: a is not defined
-@Scratchpad/1:93:1
-@Scratchpad/1:4:4
-*/
